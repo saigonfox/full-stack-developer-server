3c59ceb08ff618f53c707dcab8db157e7b9aa208
saigonfox <61135645+saigonfox@users.noreply.github.com>
2022-04-14 14:09:16 -0400

    a9 MONGO sp22

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,30 +1,37 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDao from "../tuits/tuits-dao.js";
 
-const createTuit = (req, res) => {
+// import posts from "./tuits.js";
+// let tuits = posts;
+
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
-    newTuit.stats = {likes: 0, dislikes: 0};
-    newTuit['avatar-image'] = "../../images/elon_musk.jpg";
-    tuits.unshift(newTuit); //adding the data at the top of the array
+    // newTuit._id = (new Date()).getTime()+'';
+    // newTuit.stats = {likes: 0, dislikes: 0};
+    newTuit.likes = 0;
+    // newTuit['avatar-image'] = "../../images/elon-mush.jpg";
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    // tuits.unshift(newTuit); //adding the data at the top of the array
     // tuits.push(newTuit); //adding the data at the end of the array
-    res.json(newTuit);
+    res.json(insertedTuit);
 
 }
-const findAllTuits = (req, res) => {
+const findAllTuits = async (req, res) => {
+    const tuits = await tuitsDao.findAllTuits()
     res.json(tuits);
 }
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tuitIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
-    tuits = tuits.map(t => t._id === tuitIdToUpdate ? updatedTuit : t);
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updatedTuit);
+    // tuits = tuits.map(t => t._id === tuitIdToUpdate ? updatedTuit : t);
+    res.send(status);
 
 }
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitIdToDelete = req.params.tid;
-    tuits = tuits.filter(t => t._id !== tuitIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitIdToDelete);
+    // tuits = tuits.filter(t => t._id !== tuitIdToDelete);
+    res.send(status);
 
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // const express = require('express');
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
 import helloController
     from "./controllers/hello-controller.js";
 import userController   from "./controllers/users-controller.js";
 import tuitController from "./controllers/tuits-controller.js";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
